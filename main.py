c03d0e9fd3463e0de8265a48c6852d45fb7c1c98
CarstenSavage47 <rampagev2@gmail.com>
2022-10-02 22:59:20 -0700

    Adding functions

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import openpyxl
 Telco = pandas.read_excel('/Users/carstenjuliansavage/Desktop/Telco_customer_churn.xlsx')
 
 (Telco
- .rename(columns={'Churn Reason':"Churn_Reason"})
+ .filter(['Churn Reason','City','Count','Tenure Months','Total Charges'])
+ .rename(columns={'Churn Reason':"Churn_Reason",'Tenure Months':"Tenure_Months"})
  .dropna()
  .query("Churn_Reason.str.lower().str.contains('better')",engine="python")
  .query("Churn_Reason.str.lower().str.startswith('')", engine="python")
@@ -13,9 +14,43 @@ Telco = pandas.read_excel('/Users/carstenjuliansavage/Desktop/Telco_customer_chu
  # .query("Churn_Reason.str.lower().str.contains('better&Competitor')", engine="python") # Note, the & operator does not work.
  .query('City not in ["Columbus"]')
  .query('City in ["Los Angeles","San Francisco"]')
-
+ .groupby('City')
+ .agg({'Count':"sum",'Tenure_Months':"mean",'Total Charges':"sum",'City':pandas.Series.nunique})
  )
 
+(Telco
+ .agg({'City': pandas.Series.nunique})
+ )
+
+California = (Telco
+              .query("State.str.contains('California')", engine="python")
+              .sort_values('City',ascending=True)
+              #.query('Contract.isna()')
+              .query('Contract.notnull()')
+              .assign(Nonsensical=lambda a: (a.Count+a.Latitude)/a.Longitude)
+              )
+
+City_List = ['Walnut','Diamond Bar','Rowland Heights']
+
+WDR = (Telco
+       .query('City in @City_List')
+       .dropna(subset=['Latitude','Churn Reason']) # Dropping NAs in specific columns
+       )
 
 
+
+
+# Pulling data from Star Wars API
+import requests
+import json
+import ijson
+from pandas.io.json import json_normalize
+
+response = requests.get("https://swapi.dev/api/people")
+response.json()
+print(response.text)
+response.json().keys()
+response.json()['results']
+People_JSON = response.json()['results']
+People = pandas.json_normalize(People_JSON)
 
