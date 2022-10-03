cd95409a59cd2c6e109c8d859faf876a996bd234
CarstenSavage47 <rampagev2@gmail.com>
2022-10-02 22:34:34 -0700

    Adding functions

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ Telco = pandas.read_excel('/Users/carstenjuliansavage/Desktop/Telco_customer_chu
  .query("Churn_Reason.str.lower().str.contains('better')",engine="python")
  .query("Churn_Reason.str.lower().str.startswith('')", engine="python")
  .query("Churn_Reason.str.lower().str.endswith('r')", engine="python")
+ .query("Churn_Reason.str.lower().str.contains('better|Competitor')", engine="python") # 'Or' operator
+ # .query("Churn_Reason.str.lower().str.contains('better&Competitor')", engine="python") # Note, the & operator does not work.
+ .query('City not in ["Columbus"]')
  .query('City in ["Los Angeles","San Francisco"]')
- .query("Churn_Reason.str.lower().str.contains('better|Competitor')", engine="python")
- #.query("Churn_Reason.str.lower().str.contains('better&Competitor')", engine="python") # Note, the & operator does not work.
- 
+
  )
 
 
