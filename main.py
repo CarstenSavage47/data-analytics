6bfa1308b0994298b88904e3ab082b1c7f9180fd
CarstenSavage47 <rampagev2@gmail.com>
2022-10-27 13:42:47 -0700

    Editing functions and dataframes

--- main.py
+++ main.py
@@ -164,19 +164,3 @@ Orders = pandas.read_csv('/Users/carstenjuliansavage/PycharmProjects/Random_Proj
 Employee_Orders = Employees.merge(Orders, how='inner', left_on='id', right_on='id'#, suffixes=('_Employee', '_Orders')
                                   )
 
-
-# Pulling data from Star Wars API Example
-
-import requests
-import json
-import ijson
-from pandas.io.json import json_normalize
-
-response = requests.get("https://swapi.dev/api/people")
-response.json()
-print(response.text)
-response.json().keys()
-response.json()['results']
-People_JSON = response.json()['results']
-People = pandas.json_normalize(People_JSON)
-
