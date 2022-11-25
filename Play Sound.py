24a14e3f3e17ea584ae1060d562c971c004cc5e8
CarstenSavage47 <rampagev2@gmail.com>
2022-11-25 07:29:30 -0800

    Editing functions and dataframes

--- Play Sound.py
+++ Play Sound.py
@@ -1,8 +1,11 @@
 from gtts import gTTS
 import os
-from playsound import playsound
+from pygame import mixer
 Text = "C'est fini"
 language = 'fr'
 output = gTTS(text=Text,lang=language,slow=False)
-output.save('output.mp4')
-playsound('output.mp4')
+output.save('output.mp3')
+
+mixer.init()
+mixer.music.load('/Users/carstenjuliansavage/PycharmProjects/Random_Project/output.mp3')
+mixer.music.play()
